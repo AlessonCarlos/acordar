b3afce33a7af1e27c73294c042b868d833b2a236
Alesson <alesson.carlos5@gmail.com>
2023-10-17 10:59:51 -0300

    controle de fluxo

--- TarefaCalculoMedio.java
+++ TarefaCalculoMedio.java
@@ -4,6 +4,9 @@ public class TarefaCalculoMedio {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
 
         System.out.println("Digite a primeira nota: ");
         double nota1 = scanner.nextDouble();
@@ -22,8 +25,27 @@ public class TarefaCalculoMedio {
 
         System.out.printf("A média das notas é: %.2f%n", media);
 
-        scanner.close();
+        if (media >= 7.0) {
+            System.out.println("Aluno aprovado");
+        } else if (media >= 5.0 && media <= 6.9) {
+            System.out.println("Esse aluno irá para recuperação");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+
+        
+        
+            System.out.println("Deseja inserir mais notas? (s/n): ");
+            String resposta = scanner.next();
+
+            if (resposta.equalsIgnoreCase("n")) {
+                continuar = false;
+                System.out.println("Obrigado por usar o sistema de notas.");
+            }
+            scanner.close();
+        }
     }
 }
+
 
 
